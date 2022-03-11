af9c8aec3d98ee2f892dbb49fec1c6c22ec3b0ef
meihknav <vankhiemhtpc82002@gmail.com>
2022-03-11 21:45:02 +0700

    video2

--- video2/video2.js
+++ video2/video2.js
@@ -3,3 +3,11 @@ var n = 10;
 for(var i =0;i <= n;i++ ){
     console.log('i=',i)
 }
+
+var x = 4, y =5 , z=6
+
+var x1 = 'a' + x + y;
+var x2 = x + y +'a';
+
+console.log( x1)
+console.log(x2)// cả 2 đều là string
